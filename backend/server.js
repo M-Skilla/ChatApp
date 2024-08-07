eb773b717d4d91f10e4a9f64ae558b276177ae5c
M-Skilla <skillamarko@gmail.com>
2024-08-07 12:50:35 +0300

    Final Deployment

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,13 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config();
 // const app = express();
 const PORT = process.env.PORT || 4501;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
